5f42465fc7d0c7c0678b23c6e4625d66e9207953
shubhi <shubhijain0606@gmail.com>
2019-07-16 16:08:25 +0530

    submitted task 1 for spring mvc

--- src/main/java/com.stackroute.user/User.java
+++ src/main/java/com.stackroute.user/User.java
@@ -3,27 +3,9 @@ package com.stackroute.user;
 public class User {
 
     // properties of user
-    private String userName;
-    private String userPassword;
     private String message;
 
     // getter setter
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
     public String getMessage() {
         return message;
     }
--- src/main/java/com.stackroute.user/UserController.java
+++ src/main/java/com.stackroute.user/UserController.java
@@ -9,25 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 // set the url to /user in request mapping
-@RequestMapping(value = "/user", method= RequestMethod.POST)
+@RequestMapping(value = "/", method= RequestMethod.GET)
 
 // this method set the values from form to model
-public String display(@RequestParam("userName") String userName,
-                      @RequestParam("userPassword") String userPassword, Model model){
+public String display(Model model){
 
     // create object of user class
     User user=new User();
-    // set name from the form to model
-    user.setUserName(userName.toString());
-    // set password from the form to model
-    user.setUserPassword(userPassword.toString());
     // set message
     user.setMessage("Welcome to stackroute ");
 
     // add attributes to model
-    model.addAttribute("message",user.getMessage()+user.getUserName());
+    model.addAttribute("message",user.getMessage());
     // return user
-    return "User";
+    return "index";
 }
 
 }
